c0a0cd8c3955bf6f7028c123b91d5a7c6cb23b39
Louis Chatriot <louis.chatriot@gmail.com>
2013-05-02 11:45:23 +0200

    Able to query multiple documens

--- test/db.test.js
+++ test/db.test.js
@@ -6,6 +6,7 @@ var Datastore = require('../lib/datastore')
   , should = require('chai').should()
   , assert = require('chai').assert
   , _ = require('underscore')
+  , async = require('async')
   ;
 
 
@@ -20,63 +21,127 @@ describe('Database', function () {
     });
   });
 
-  it('Able to insert a document in the database and retrieve it even after a reload', function (done) {
-    var d = new Datastore(testDb);
-    d.loadDatabase(function (err) {
-      assert.isNull(err);
-      d.find({}, function (err, docs) {
-        docs.length.should.equal(0);
+  describe('Insert', function () {
 
-        d.insert({ somedata: 'ok' }, function (err) {
-          // The data was correctly updated
-          d.find({}, function (err, docs) {
-            assert.isNull(err);
-            docs.length.should.equal(1);
-            Object.keys(docs[0]).length.should.equal(2);
-            docs[0].somedata.should.equal('ok');
-            assert.isDefined(docs[0]._id);
+    it('Able to insert a document in the database and retrieve it even after a reload', function (done) {
+      var d = new Datastore(testDb);
+      d.loadDatabase(function (err) {
+        assert.isNull(err);
+        d.find({}, function (err, docs) {
+          docs.length.should.equal(0);
 
-            // After a reload the data has been correctly persisted
-            d.loadDatabase(function (err) {
-              d.find({}, function (err, docs) {
-                assert.isNull(err);
-                docs.length.should.equal(1);
-                Object.keys(docs[0]).length.should.equal(2);
-                docs[0].somedata.should.equal('ok');
-                assert.isDefined(docs[0]._id);
+          d.insert({ somedata: 'ok' }, function (err) {
+            // The data was correctly updated
+            d.find({}, function (err, docs) {
+              assert.isNull(err);
+              docs.length.should.equal(1);
+              Object.keys(docs[0]).length.should.equal(2);
+              docs[0].somedata.should.equal('ok');
+              assert.isDefined(docs[0]._id);
+
+              // After a reload the data has been correctly persisted
+              d.loadDatabase(function (err) {
+                d.find({}, function (err, docs) {
+                  assert.isNull(err);
+                  docs.length.should.equal(1);
+                  Object.keys(docs[0]).length.should.equal(2);
+                  docs[0].somedata.should.equal('ok');
+                  assert.isDefined(docs[0]._id);
 
 
-                done();
+                  done();
+                });
               });
             });
           });
         });
       });
     });
-  });
 
-  it('Can insert multiple documents in the database', function (done) {
-    var d = new Datastore(testDb);
-    d.loadDatabase(function (err) {
-      assert.isNull(err);
-      d.find({}, function (err, docs) {
-        docs.length.should.equal(0);
+    it('Can insert multiple documents in the database', function (done) {
+      var d = new Datastore(testDb);
+      d.loadDatabase(function (err) {
+        assert.isNull(err);
+        d.find({}, function (err, docs) {
+          docs.length.should.equal(0);
 
-        d.insert({ somedata: 'ok' }, function (err) {
-          d.insert({ somedata: 'another' }, function (err) {
-            d.insert({ somedata: 'again' }, function (err) {
-              d.find({}, function (err, docs) {
-                docs.length.should.equal(3);
-                _.pluck(docs, 'somedata').should.contain('ok');
-                _.pluck(docs, 'somedata').should.contain('another');
-                _.pluck(docs, 'somedata').should.contain('again');
-                done();
+          d.insert({ somedata: 'ok' }, function (err) {
+            d.insert({ somedata: 'another' }, function (err) {
+              d.insert({ somedata: 'again' }, function (err) {
+                d.find({}, function (err, docs) {
+                  docs.length.should.equal(3);
+                  _.pluck(docs, 'somedata').should.contain('ok');
+                  _.pluck(docs, 'somedata').should.contain('another');
+                  _.pluck(docs, 'somedata').should.contain('again');
+                  done();
+                });
               });
             });
           });
         });
       });
     });
-  });
+
+  });   // ==== End of 'Insert' ==== //
+
+
+  describe('Find', function () {
+
+    it('Can find all documents an empty query is used', function (done) {
+      var d = new Datastore(testDb);
+
+      async.waterfall([
+      function (cb) {
+        d.loadDatabase(function (err) {
+          d.insert({ somedata: 'ok' }, function (err) {
+            d.insert({ somedata: 'another', plus: 'additional data' }, function (err) {
+              d.insert({ somedata: 'again' }, function (err) { return cb(err); });
+            });
+          });
+        });
+      }
+      , function (cb) {   // Test with empty object
+        d.find({}, function (err, docs) {
+          assert.isNull(err);
+          docs.length.should.equal(3);
+          _.pluck(docs, 'somedata').should.contain('ok');
+          _.pluck(docs, 'somedata').should.contain('another');
+          _.find(docs, function (d) { return d.somedata === 'another' }).plus.should.equal('additional data');
+          _.pluck(docs, 'somedata').should.contain('again');
+          return cb();
+        });
+      }
+      ], done);
+    });
+
+    it('Can find all documents matching a basic query', function (done) {
+      var d = new Datastore(testDb);
+
+      async.waterfall([
+      function (cb) {
+        d.loadDatabase(function (err) {
+          d.insert({ somedata: 'ok' }, function (err) {
+            d.insert({ somedata: 'again', plus: 'additional data' }, function (err) {
+              d.insert({ somedata: 'again' }, function (err) { return cb(err); });
+            });
+          });
+        });
+      }
+      , function (cb) {   // Test with empty object
+        d.find({ somedata: 'again' }, function (err, docs) {
+          assert.isNull(err);
+          docs.length.should.equal(2);
+          _.pluck(docs, 'somedata').should.not.contain('ok');
+          return cb();
+        });
+      }
+      ], done);
+    });
+
+
+
+  });   // ==== End of 'Find' ==== //
+
+
 
 });
