d3d38b08204e216131cb79554ccf028439d37d01
Louis Chatriot <louis.chatriot@gmail.com>
2013-07-16 19:52:08 +0200

    Update tests done

--- browser-version/test/nedb-browser.js
+++ browser-version/test/nedb-browser.js
@@ -1,5 +1,19 @@
+/**
+ * Testing the browser version of NeDB
+ * The goal of these tests is not to be exhaustive, we have the server-side NeDB tests for that
+ * This is more of a sanity check which executes most of the code at least once and checks
+ * it behaves as the server version does
+ */
+
 var assert = chai.assert;
 chai.should();
+
+/**
+ * Given a docs array and an id, return the document whose id matches, or null if none is found
+ */
+function findById (docs, id) {
+  return _.find(docs, function (doc) { return doc._id === id; }) || null;
+}
 
 
 describe('Basic CRUD functionality', function () {
@@ -86,10 +100,56 @@ describe('Basic CRUD functionality', function () {
 
     db.insert({ planet: 'Eaaaaarth' }, function (err, newDoc1) {
       db.insert({ planet: 'Maaaaars' }, function (err, newDoc2) {
+        // Simple update
         db.update({ _id: newDoc2._id }, { $set: { planet: 'Saturn' } }, {}, function (err, nr) {
           assert.isNull(err);
           nr.should.equal(1);
-          done();
+
+          db.find({}, function (err, docs) {
+            docs.length.should.equal(2);
+            findById(docs, newDoc1._id).planet.should.equal('Eaaaaarth');
+            findById(docs, newDoc2._id).planet.should.equal('Saturn');
+
+            // Failing update
+            db.update({ _id: 'unknown' }, { $inc: { count: 1 } }, {}, function (err, nr) {
+              assert.isNull(err);
+              nr.should.equal(0);
+
+              db.find({}, function (err, docs) {
+                docs.length.should.equal(2);
+                findById(docs, newDoc1._id).planet.should.equal('Eaaaaarth');
+                findById(docs, newDoc2._id).planet.should.equal('Saturn');
+
+                // Document replacement
+                db.update({ planet: 'Eaaaaarth' }, { planet: 'Uranus' }, { multi: false }, function (err, nr) {
+                  assert.isNull(err);
+                  nr.should.equal(1);
+
+                  db.find({}, function (err, docs) {
+                    docs.length.should.equal(2);
+                    findById(docs, newDoc1._id).planet.should.equal('Uranus');
+                    findById(docs, newDoc2._id).planet.should.equal('Saturn');
+
+                    // Multi update
+                    db.update({}, { $inc: { count: 3 } }, { multi: true }, function (err, nr) {
+                      assert.isNull(err);
+                      nr.should.equal(2);
+
+                      db.find({}, function (err, docs) {
+                        docs.length.should.equal(2);
+                        findById(docs, newDoc1._id).planet.should.equal('Uranus');
+                        findById(docs, newDoc1._id).count.should.equal(3);
+                        findById(docs, newDoc2._id).planet.should.equal('Saturn');
+                        findById(docs, newDoc2._id).count.should.equal(3);
+
+                        done();
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
         });
       });
     });
