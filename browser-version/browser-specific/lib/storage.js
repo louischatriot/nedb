caeb1893260a41bc9045f26e9fc19483dcc40f58
Louis Chatriot <louis.chatriot@gmail.com>
2015-11-09 12:15:02 +0100

    Use localforage instead of localStorage

--- browser-version/browser-specific/lib/storage.js
+++ browser-version/browser-specific/lib/storage.js
@@ -1,80 +1,72 @@
 /**
  * Way data is stored for this database
  * For a Node.js/Node Webkit database it's the file system
- * For a browser-side database it's localStorage when supported
+ * For a browser-side database it's localforage, which uses the best backend available (IndexedDB then WebSQL then localStorage)
  *
  * This version is the browser version
  */
 
+var localforage = require('localforage')
+
+// Configure localforage to display NeDB name for now. Would be a good idea to let user use his own app name
+localforage.config({
+  name: 'NeDB'
+, storeName: 'nedbdata'
+});
 
 
 function exists (filename, callback) {
-  // In this specific case this always answers that the file doesn't exist
-  if (typeof localStorage === 'undefined') { console.log("WARNING - This browser doesn't support localStorage, no data will be saved in NeDB!"); return callback(); }
-
-  if (localStorage.getItem(filename) !== null) {
-    return callback(true);
-  } else {
-    return callback(false);
-  }
+  localforage.getItem(filename, function (err, value) {
+    if (value !== null) {   // Even if value is undefined, localforage returns null
+      return callback(true);
+    } else {
+      return callback(false);
+    }
+  });
 }
 
 
 function rename (filename, newFilename, callback) {
-  if (typeof localStorage === 'undefined') { console.log("WARNING - This browser doesn't support localStorage, no data will be saved in NeDB!"); return callback(); }
-
-  if (localStorage.getItem(filename) === null) {
-    localStorage.removeItem(newFilename);
-  } else {
-    localStorage.setItem(newFilename, localStorage.getItem(filename));
-    localStorage.removeItem(filename);
-  }
-
-  return callback();
+  localforage.getItem(filename, function (err, value) {
+    if (value === null) {
+      localforage.removeItem(newFilename, function () { return callback(); });
+    } else {
+      localforage.setItem(newFilename, value, function () {
+        localforage.removeItem(filename, function () { return callback(); });
+      });
+    }
+  });
 }
 
 
 function writeFile (filename, contents, options, callback) {
-  if (typeof localStorage === 'undefined') { console.log("WARNING - This browser doesn't support localStorage, no data will be saved in NeDB!"); return callback(); }
-
   // Options do not matter in browser setup
   if (typeof options === 'function') { callback = options; }
-
-  localStorage.setItem(filename, contents);
-  return callback();
+  localforage.setItem(filename, contents, function () { return callback(); });
 }
 
 
 function appendFile (filename, toAppend, options, callback) {
-  if (typeof localStorage === 'undefined') { console.log("WARNING - This browser doesn't support localStorage, no data will be saved in NeDB!"); return callback(); }
-
   // Options do not matter in browser setup
   if (typeof options === 'function') { callback = options; }
 
-  var contents = localStorage.getItem(filename) || '';
-  contents += toAppend;
-
-  localStorage.setItem(filename, contents);
-  return callback();
+  localforage.getItem(filename, function (err, contents) {
+    contents = contents || '';
+    contents += toAppend;
+    localforage.setItem(filename, contents, function () { return callback(); });
+  });
 }
 
 
 function readFile (filename, options, callback) {
-  if (typeof localStorage === 'undefined') { console.log("WARNING - This browser doesn't support localStorage, no data will be saved in NeDB!"); return callback(); }
-
   // Options do not matter in browser setup
   if (typeof options === 'function') { callback = options; }
-
-  var contents = localStorage.getItem(filename) || '';
-  return callback(null, contents);
+  localforage.getItem(filename, function (err, contents) { return callback(null, contents || ''); });
 }
 
 
 function unlink (filename, callback) {
-  if (typeof localStorage === 'undefined') { console.log("WARNING - This browser doesn't support localStorage, no data will be saved in NeDB!"); return callback(); }
-
-  localStorage.removeItem(filename);
-  return callback();
+  localforage.removeItem(filename, function () { return callback(); });
 }
 
 
