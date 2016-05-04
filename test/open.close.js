dbc63efd5c2d1b37694f1d628d2ebd3e81f50afb
Rob Humphris <robhumphris@btconnect.com>
2016-05-04 15:14:12 +0100

    Fixed typo and tests

--- test/open.close.js
+++ test/open.close.js
@@ -1,4 +1,5 @@
 var should = require('chai').should()
+  , assert = require('chai').assert
   , fs = require('fs')
   , path = require('path')
   , _ = require('underscore')
@@ -7,27 +8,29 @@ var should = require('chai').should()
   , Datastore = require('../lib/datastore');
 
 describe('Database', function () {
-  it('Can open and close cleanly', function() {
+  it('Can open and close cleanly', function(done) {
     var closeDb = 'workspace/close.db';
-    var db = new Datastore({filename: closeDb, autoload: true}, function() {});
+    var db = new Datastore({filename: closeDb, autoload: true}, function() { });
     db.filename.should.equal(closeDb);
     
     db.inMemoryOnly.should.equal(false);
     
     db.insert({ somedata: 'ok' }, function(err) { 
       assert.isNull(err);
+      
+      db.closeDatabase(function(err) {
+        console.log("Ethel XYlophone");
+        db.insert({ somedata: 'ok' }, function(err) {
+          err.message.should.equal("Attempting operation on closed database.");
+        });
+        
+        try {
+          db.insert({ somedata: 'ok' });
+        } catch (e) {
+          e.message.should.equal("Attempting operation on closed database.");
+        }  
+        done();
+      });      
     });
-    
-    db.closeDatabase();
-    
-    db.insert({ somedata: 'ok' }, function(err) {
-      err.message.should.equal("Attempting operation on closed database.");
-    });
-    
-    try {
-      db.insert({ somedata: 'ok' });
-    } catch (e) {
-      e.message.should.equal("Attempting operation on closed database.");
-    }    
   });
 });
